637f0c8e9aa2a016c38070700bb547293b9d3337
juanpa12121 <juan_rivera82172@elpoli.edu.co>
2023-04-19 22:03:14 -0500

    Implementacion formulario sin el Widget de Form

--- lib/pages/29_my_first_page.dart
+++ lib/pages/29_my_first_page.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_myapp/pages/29_my_second_page.dart';
 
-class MyFirstPage extends StatelessWidget {
+class MyFirstPage extends StatefulWidget {
   const MyFirstPage({Key? key}) : super(key: key);
+
+  @override
+  State<MyFirstPage> createState() => _MyFirstPageState();
+}
+
+class _MyFirstPageState extends State<MyFirstPage> {
+  //Definir controladores
+  late TextEditingController nameTextController = TextEditingController();
+  late TextEditingController lastNameTextController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -11,16 +20,54 @@ class MyFirstPage extends StatelessWidget {
         title: const Text("First Route"),
       ),
       body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              _showSecondPage(context);
-            },
-            child: const Text("Open second page")),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            //Alineado vertical
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Nombre',
+                ),
+                controller: nameTextController,
+              ),
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Apellido',
+                ),
+                controller: lastNameTextController,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    _showSecondPage(context);
+                  },
+                  child: const Text("Open second page")),
+            ],
+          ),
+        ),
       ),
     );
   }
 
   void _showSecondPage(BuildContext context) {
-    Navigator.pushNamed(context, "/my_second_page", arguments: "GO HOME");
+    Navigator.pushNamed(context, "/my_second_page",
+        arguments: SecondPageArguments(
+            nameTextController.text, lastNameTextController.text));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    nameTextController = TextEditingController();
+    lastNameTextController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    nameTextController.dispose();
+    lastNameTextController.dispose();
+    super.dispose();
   }
 }
--- lib/pages/29_my_second_page.dart
+++ lib/pages/29_my_second_page.dart
@@ -6,27 +6,47 @@ class MySecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //Obtener argumentos del Widget my_firts_page
-    final String name = ModalRoute.of(context)!.settings.arguments as String;
+    final name = ModalRoute.of(context)!.settings.arguments as SecondPageArguments?;
+    SecondPageArguments arguments = ModalRoute.of(context)!.settings.arguments as SecondPageArguments;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Second Route"),
         actions: [
-          Text("Hello $name"),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Text("Hello ${arguments.name}", style: TextStyle(fontSize: 20,)),
+          ),
         ],
       ),
       body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: const Text("Go back!")),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          //Alineado vertical
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Go back!")),
+            Text(arguments.name.toString()),
+            Text(arguments.lastName.toString()),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.pushNamed(context, "/home");
         },
-        child: Text(name),
+        child: Text(name.toString()),
       )
     );
   }
+}
+
+class SecondPageArguments {
+  final String? name;
+  final String? lastName;
+
+  SecondPageArguments(this.name, this.lastName);
 }
